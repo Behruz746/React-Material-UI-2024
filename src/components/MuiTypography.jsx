9c42b88212163dadc61461989e4f59fd555eafbd
Behruz <madaminovbehruz746@gmail.com>
2024-07-24 23:36:32 +0500

    added: how create text with Typography tag

---
+++ src/components/MuiTypography.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function MuiTypography() {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        h4 Heading
+      </Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">subtitle1 Heading</Typography>
+      <Typography variant="subtitle2">subtitle2 Heading</Typography>
+
+      <Typography variant="body1">
+        body1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+        officiis!
+      </Typography>
+      <Typography variant="body2">
+        body2 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </Typography>
+
+      <Typography variant="button">button Heading</Typography>
+
+      <Typography variant="inherit">inherit Heading</Typography>
+
+      <Typography variant="overline">overline Heading</Typography>
+
+      <Typography variant="caption">caption Heading</Typography>
+    </div>
+  );
+}
+
+export default MuiTypography;
