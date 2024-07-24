8627c66f30521622aa5b9baf1f27709797ed7831
Behruz <madaminovbehruz746@gmail.com>
2024-07-25 00:37:14 +0500

    added: how create Button and attributs tag with MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import MuiTypography from "./components/MuiTypography";
+import { MuiTypography, MuiButton } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
+      {/* <h1>Typography tags</h1> */}
+      {/* <MuiTypography /> */}
+      <br />
+      <h1>Button tags</h1>
+      <MuiButton />
     </div>
   );
 }
--- src/components/MuiTypography.jsx
+++ src/components/MuiTypography.jsx
@@ -8,7 +8,7 @@ function MuiTypography() {
       <Typography variant="h2">h2 Heading</Typography>
       <Typography variant="h3">h3 Heading</Typography>
       <Typography variant="h4" component="h1" gutterBottom>
-        h4 Heading
+        h4 style Heading h1 gap and margin bottom 0.35em)
       </Typography>
       <Typography variant="h5">h5 Heading</Typography>
       <Typography variant="h6">h6 Heading</Typography>
